f1e72761082bbc015dc66d22d7579ea615575fe1
kas <32327548+kaspar1ndustries@users.noreply.github.com>
2023-03-26 14:38:42 +0800

    derive key from a file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use hkdf::Hkdf;
+use sha2::Sha256;
 use std::env;
-const VERSION: &'static str = env!("CARGO_PKG_VERSION");
-const NAME: &'static str = env!("CARGO_PKG_NAME");
+use std::fs::File;
+use std::io::Read;
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+const NAME: &str = env!("CARGO_PKG_NAME");
 fn main() {
     // get input from user
     let file_key = env::args().nth(1);
@@ -16,4 +20,28 @@ fn main() {
     let file_to_encrypt = file_to_encrypt.unwrap();
     println!("File key: {}", file_key);
     println!("File to encrypt: {}", file_to_encrypt);
+    // read files
+    let key = read_file(&file_key);
+    println!("key file is {} bytes", key.len());
+    let salt = b"some_salt";
+    let password = derive_key(&key, salt, 32);
+    let password_hex = hex::encode(password);
+    println!("aes key for the file is {}", password_hex);
+}
+
+/// read file and return it as a vector of bytes
+fn read_file(filename: &str) -> Vec<u8> {
+    let mut file = File::open(filename).unwrap();
+    let mut buffer = Vec::new();
+    // read the file to the buffer
+    file.read_to_end(&mut buffer).unwrap();
+    buffer
+}
+
+/// derive a key from a file bytes
+fn derive_key(input: &[u8], salt: &[u8], output_key_len: usize) -> Vec<u8> {
+    let hkdf = Hkdf::<Sha256>::new(Some(salt), input);
+    let mut key = vec![0u8; output_key_len];
+    hkdf.expand(&[], &mut key).expect("Error deriving key");
+    key
 }
